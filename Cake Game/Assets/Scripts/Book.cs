52c2f631b54184507144edb17f9c1b091ac05d2e
Purn <purnjay19@gmail.com>
2020-04-11 11:14:24 +1200

    1st commit

---
+++ Cake Game/Assets/Scripts/Book.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Book : MonoBehaviour
+{
+    GridLayout grid;
+    public int maxCakes = 5;
+
+    string addcake = "2";
+
+    void Update()
+    {
+
+        if (transform.childCount < maxCakes)
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                AddCake(addcake);
+            }
+        }
+        else {
+            Debug.Log("Collection is full!");
+        }
+
+    }
+
+    public void AddCake(string id)
+    {
+        var CakeToAdd = Resources.Load<GameObject>(id);
+        StartCoroutine(CakeSpawn(CakeToAdd));
+    }
+
+    IEnumerator CakeSpawn(GameObject cakeToAdd)
+    {
+        Instantiate(cakeToAdd, transform.position, transform.rotation, this.transform);
+        Debug.Log("Child Count:" + transform.childCount);
+        yield return null;
+    }
+
+ }
---
+++ Cake Game/Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float speed = 3f;
+
+    Rigidbody2D rb2d;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.gravityScale = 0f;
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        rb2d.velocity = new Vector2(h * speed, v * speed);
+    }
+
+
+}
